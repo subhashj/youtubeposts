d4f6deafc0a134aa1ba64b9d21a55d287fac6800
Subhash Jha <subhashj@adobe.com>
2012-10-05 18:48:25 +0530

    Base YouTubePosts Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youtubeposts::Application.config.secret_token = '2b67b5659e6d409d956dfdafefc175f0008257557353f27f078310f296af2ec88ce7d50e9e62fc94619c5d090ceef75e941fcad3ed7ce6c944760239d69d27a6'
